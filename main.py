e48c2fb7930a6cd7e8556828effab30fd0451847
felipesarian <felipe.sarian@gmail.com>
2024-05-13 11:18:56 -0300

    torre2

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ import pygame
 # pygame setup
 pygame.init()
 pygame.display.set_caption('loonbs')
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((500, 400))
 clock = pygame.time.Clock()
 running = True
 
+screen.fill((90,170,20))
 while running:
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 coloca_torre(screen,(pygame.Vector2(50,50)))
-    screen.fill((90,170,20))
+    
 
     pygame.display.flip()
     pygame.display.update()
