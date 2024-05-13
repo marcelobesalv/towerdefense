7801ccb6c7383c7a1ff185d1b5a22159a4c347e3
marcelobesalv <alvmbessa@gmail.com>
2024-05-13 10:23:53 -0300

    func e tela

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 #teste
 #teste2
+import pygame
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
