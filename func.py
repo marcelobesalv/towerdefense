5df8e4f2c4941c5b7ffcf2a025098b03fc7b52de
marcelobesalv <alvmbessa@gmail.com>
2024-05-13 11:34:18 -0300

    render

--- func.py
+++ func.py
@@ -1,4 +1,7 @@
 import pygame
-def coloca_torre(surf,pos):
+def coloca_torre(surf,d):
+    for torre,lpos in d.items():
+        for pos in lpos:
+            pygame.draw.circle(surf,(0,0,0),pos,50)
 
-    pygame.draw.circle(surf,(0,0,0),pos,50)
+
