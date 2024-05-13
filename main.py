44ff054ee00d373da953dcb7ed3b6ed6f83577f1
marcelobesalv <alvmbessa@gmail.com>
2024-05-13 10:55:40 -0300

    torre

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from func import *
 import pygame
 # pygame setup
 pygame.init()
+pygame.display.set_caption('loonbs')
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
@@ -13,5 +14,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            coloca_torre(screen,0,0)
-    screen.fill('green')
+        
+            coloca_torre(screen,50,50)
+    screen.fill((90,170,20))
+    pygame.display.flip()
+    pygame.display.update()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
