d35a17b7debdf67586eb05834339424915643dfa
felipesarian <felipe.sarian@gmail.com>
2024-05-15 09:04:25 -0300

    centralizando torres no mouse

--- func.py
+++ func.py
@@ -2,11 +2,15 @@ import pygame
 def coloca_torre(surf,d):
     for torre,lpos in d.items():
         if torre == 'bola':
+            diametro = 100
             for pos in lpos:
-                pygame.draw.circle(surf,(0,0,0),pos,50)
+                pygame.draw.circle(surf,(0,0,0),pos,diametro/2)
         if torre == 'quadrado':
+            lado = 100
             for pos in lpos:
-                pygame.draw.rect(surf,(0,0,0), pygame.Rect(pos[0], pos[1], 100, 100))
+                pygame.draw.rect(surf,(0,0,0), pygame.Rect(pos[0]-lado/2, pos[1]-lado/2, lado, lado))
         if torre == 'triangulo':
+            lado = 100
+            altura = lado*(3**(1/2))/2
             for pos in lpos:
-                pygame.draw.polygon(surf,(0,0,0), [pos, (pos[0]+50, pos[1]-100), (pos[0]+100, pos[1])])
+                pygame.draw.polygon(surf,(0,0,0), [(pos[0]-lado/2, pos[1]+altura/3), (pos[0]+lado/2, pos[1]+altura/3), (pos[0], pos[1]-altura*2/3)])
