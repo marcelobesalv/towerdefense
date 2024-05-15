a1e74785acf93f5c513da1d9803a5f44e8f27a2e
felipesarian <felipe.sarian@gmail.com>
2024-05-15 08:01:50 -0300

    remove bola

--- main.py
+++ main.py
@@ -10,18 +10,22 @@ clock = pygame.time.Clock()
 running = True
 torres = {} #{tipo: [(x, y), (x2, y2), ...]}
 torres['bola'] = []
-
+clear = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE: #bota torre bola
                 torres['bola'].append((50, 50))
-
-    #render
+            if event.key == pygame.K_BACKSPACE: #remove ultima torre bola
+                print(torres['bola'])               
+                torres['bola'] = torres['bola'][:-1]
+                print(torres['bola'])               
+    #----------------------------------------render------------------------------------------------------
     screen.fill((90,170,20))
     coloca_torre(screen, torres)
+ 
     
 
 
