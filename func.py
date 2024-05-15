34663e1d49ede3bdc6a6d20ccead86e8c638ea3c
felipesarian <felipe.sarian@gmail.com>
2024-05-15 08:19:14 -0300

    nova torre quadrada

--- func.py
+++ func.py
@@ -1,7 +1,9 @@
 import pygame
 def coloca_torre(surf,d):
     for torre,lpos in d.items():
-        for pos in lpos:
-            pygame.draw.circle(surf,(0,0,0),pos,50)
-
-
+        if torre == 'bola':
+            for pos in lpos:
+                pygame.draw.circle(surf,(0,0,0),pos,50)
+        if torre == 'quadrado':
+            for pos in lpos:
+                pygame.draw.rect(surf,(0,0,0), pygame.Rect(pos[0], pos[1], 100, 100))
--- main.py
+++ main.py
@@ -8,20 +8,33 @@ pygame.display.set_caption('loonbs')
 screen = pygame.display.set_mode((500, 400))
 clock = pygame.time.Clock()
 running = True
-torres = {} #{tipo: [(x, y), (x2, y2), ...]}
-torres['bola'] = []
+torres = {'bola': [], 'quadrado': [], 'triangulo': []} #{tipo: [(x, y), (x2, y2), ...]}
 clear = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE: #bota torre bola
+            if event.key == pygame.K_1: #bota torre bola
                 torres['bola'].append((50, 50))
-            if event.key == pygame.K_BACKSPACE: #remove ultima torre bola
-                print(torres['bola'])               
+                print('bolas', torres['bola'])
+            if event.key == pygame.K_q: #remove ultima torre bola
                 torres['bola'] = torres['bola'][:-1]
-                print(torres['bola'])               
+                print('bolas', torres['bola'])   
+
+            if event.key == pygame.K_2: #bota torre quadrado
+                torres['quadrado'].append((100, 0))
+                print('quadrados', torres['quadrado'])
+            if event.key == pygame.K_w: #remove ultima torre quadrado
+                torres['quadrado'] = torres['quadrado'][:-1]
+                print('quadrados', torres['quadrado'])
+
+            if event.key == pygame.K_1: #bota torre bola
+                torres['bola'].append((50, 50))
+                print('bolas', torres['bola'])
+            if event.key == pygame.K_q: #remove ultima torre bola
+                torres['bola'] = torres['bola'][:-1]
+                print('bolas', torres['bola'])            
     #----------------------------------------render------------------------------------------------------
     screen.fill((90,170,20))
     coloca_torre(screen, torres)
