11efebd591fe84679d68f9085eda9bf2f7e7d7b6
Felipe Aldrighi de Lima <159392090+felipeal15@users.noreply.github.com>
2024-05-21 21:16:52 -0300

    Ajustando as diferentes torres
    Ajustei os botoes do lado do jogo, conseguindo selecionar cada torre, ainda sao necessarias as imagens e ajustar a questao da colocacao estar completamente estranha

--- torretas.py
+++ torretas.py
@@ -1,8 +1,9 @@
 import pygame
 
 class Torres(pygame.sprite.Sprite):
-    def __init__(self, image, pos):
+    def __init__(self, image, pos, tipo_torre):
+        super().__init__()
         pygame.sprite.Sprite.__init__(self)
         self.image = image
-        self.rect = self.image.get_rect()
-        self.rect.center = pos
+        self.rect = self.image.get_rect(center=pos)
+        self.tipo_torre = tipo_torre
