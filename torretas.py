ced57f9279d90549f3d0a7eb7ad9ec0853a0216b
marcelobesalv <alvmbessa@gmail.com>
2024-05-22 10:14:22 -0300

    rangeshot

--- torretas.py
+++ torretas.py
@@ -1,6 +1,7 @@
 import pygame
 import math
 
+
 class Torres(pygame.sprite.Sprite):
     def __init__(self, imagelist, pos, tipo_torre):
         super().__init__()
@@ -9,8 +10,8 @@ class Torres(pygame.sprite.Sprite):
         self.animationlist = imagelist
         self.framei = 0
         self.updatetime = pygame.time.get_ticks()
-
-
+        self.x = pos[0]
+        self.y = pos[1]
         self.image = self.animationlist[self.framei]
         self.rect = self.image.get_rect(center=pos)
         self.tipo_torre = tipo_torre
@@ -24,16 +25,22 @@ class Torres(pygame.sprite.Sprite):
         
 
     def escolheAlvo(self, grupo_inimigos):
+        x_dist = 0
+        y_dist = 0
         for i in grupo_inimigos:
-            if i.health > 0:
-                self.target = i
-                print('alvo')
-                if self.tipo_torre == 1:
-                    self.target.health -= 1
-                elif self.tipo_torre == 2:
-                    self.target.health -= 3
-                elif self.tipo_torre == 3:
-                    self.target.health -= 5
+            x_dist = i.pos[0] - self.x
+            y_dist = i.pos[1] - self.y    
+            dist = math.sqrt(x_dist**2+y_dist**2)
+            if dist < self.range:
+                if i.health > 0:
+                    self.target = i
+                    print('alvo')
+                    if self.tipo_torre == 1:
+                        self.target.health -= 1
+                    elif self.tipo_torre == 2:
+                        self.target.health -= 3
+                    elif self.tipo_torre == 3:
+                        self.target.health -= 5
 
     def update(self, grupo_inimigos):
         if self.target:
